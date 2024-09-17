d2c5497fdbf35e58fd43a9171829d3c3634492d6
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2024-09-16 22:57:44 -0500

    Binary file wire-up for parity algorithms
    Originally written at https://gist.github.com/zgoethel/97d0c4dd73a54a9a5c8a31c657f5aa75

---
+++ DataVault/Parity.cs
@@ -0,0 +1,133 @@
+﻿#define VECTORIZE
+
+#if VECTORIZE
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+#endif
+
+namespace DataVault;
+
+#if VECTORIZE
+using _Vector = Vector256;
+using _VectorT = Vector256<byte>;
+#endif
+
+public static class Parity
+{
+#if VECTORIZE
+    private static readonly _VectorT EVEN_BITS = _Vector.Create<byte>(0b10101010);
+    private static readonly _VectorT ODD_BITS = _Vector.Create<byte>(0b01010101);
+#else
+    private const byte EVEN_BITS = 0b10101010;
+    private const byte ODD_BITS = 0b01010101;
+#endif
+
+    public static void CalculateP(ReadOnlySpan<byte> a, ReadOnlySpan<byte> b, Span<byte> p)
+    {
+        if (a.Length != b.Length || b.Length != p.Length)
+        {
+            throw new ArgumentException("Mismatch in sizes of provided spans");
+        }
+#if VECTORIZE
+        if (a.Length % _VectorT.Count != 0)
+        {
+            throw new ArgumentException($"Span size must be evently divisible by {_VectorT.Count}");
+        }
+
+        var aVector = MemoryMarshal.Cast<byte, _VectorT>(a);
+        var bVector = MemoryMarshal.Cast<byte, _VectorT>(b);
+
+        var pVector = MemoryMarshal.Cast<byte, _VectorT>(p);
+
+        for (var i = 0; i < aVector.Length; i++)
+        {
+            pVector[i] = _Vector.Xor(aVector[i], bVector[i]);
+        }
+#else
+        for (var i = 0; i < a.Length; i++)
+        {
+            p[i] = (byte)(a[i] ^ b[i]);
+        }
+#endif
+    }
+
+    public static void RestoreAFromBAndP(ReadOnlySpan<byte> b, ReadOnlySpan<byte> p, Span<byte> a)
+    {
+        CalculateP(b, p, a);
+    }
+
+    public static void RestoreBFromAAndP(ReadOnlySpan<byte> a, ReadOnlySpan<byte> p, Span<byte> b)
+    {
+        CalculateP(a, p, b);
+    }
+
+    public static void Mangle(ReadOnlySpan<byte> b, Span<byte> mangle)
+    {
+        if (b.Length != mangle.Length)
+        {
+            throw new ArgumentException("Mismatch in sizes of provided spans");
+        }
+#if VECTORIZE
+        if (b.Length % _VectorT.Count != 0)
+        {
+            throw new ArgumentException($"Span size must be evently divisible by {_VectorT.Count}");
+        }
+
+        var bVector = MemoryMarshal.Cast<byte, _VectorT>(b);
+
+        var mangleVector = MemoryMarshal.Cast<byte, _VectorT>(mangle);
+
+        for (var i = 0; i < bVector.Length; i++)
+        {
+            var b0 = _Vector.BitwiseAnd(bVector[i], EVEN_BITS);
+            var b1 = _Vector.BitwiseAnd(bVector[i], ODD_BITS);
+
+            var _b0 = _Vector.ShiftRightLogical(b0, 1);
+            var _b1 = _Vector.ShiftLeft(b1, 1);
+
+            mangleVector[i] = _Vector.BitwiseOr(_Vector.Xor(b0, _b1), _b0);
+        }
+#else
+        for (var i = 0; i < b.Length; i++)
+        {
+            var b0 = b[i] & EVEN_BITS;
+            var b1 = b[i] & ODD_BITS;
+
+            var _b0 = b0 >>> 1;
+            var _b1 = b1 << 1;
+
+            mangle[i] = (byte)((b0 ^ _b1) | _b0);
+        }
+#endif
+    }
+
+    public static void Unmangle(ReadOnlySpan<byte> mangle, Span<byte> b)
+    {
+        Mangle(mangle, b);
+        Mangle(b, b);
+    }
+
+    public static void CalculateQ(ReadOnlySpan<byte> a, ReadOnlySpan<byte> b, Span<byte> q)
+    {
+        Mangle(b, q);
+        CalculateP(a, q, q);
+    }
+
+    public static void RestoreAFromBAndQ(ReadOnlySpan<byte> b, ReadOnlySpan<byte> q, Span<byte> a)
+    {
+        Mangle(b, a);
+        CalculateP(q, a, a);
+    }
+
+    public static void RestoreBFromAAndQ(ReadOnlySpan<byte> a, ReadOnlySpan<byte> q, Span<byte> b)
+    {
+        CalculateP(a, q, b);
+        Unmangle(b, b);
+    }
+
+    public static void RestoreBFromPAndQ(ReadOnlySpan<byte> p, ReadOnlySpan<byte> q, Span<byte> b)
+    {
+        CalculateP(p, q, b);
+        Mangle(b, b);
+    }
+}
---
+++ DataVault/Program.cs
@@ -0,0 +1,265 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DataVault;
+
+internal static class Program
+{
+    private static readonly int PARITY_SIZE = 4096;
+
+    static async Task<int> Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.Error.WriteLine("Usage: ./DataVault.exe file_name");
+            return 1;
+        }
+        var fileName = args[0];
+
+        var buffer = new byte[PARITY_SIZE * 2];
+
+        var pBuffer = new byte[PARITY_SIZE];
+        var qBuffer = new byte[PARITY_SIZE];
+
+        byte[] createSizeBytes(long size)
+        {
+            var _size = new[] { size };
+            return MemoryMarshal.Cast<long, byte>(_size).ToArray();
+        }
+
+        long createSize(ReadOnlySpan<byte> sizeBytes)
+        {
+            var _size = MemoryMarshal.Cast<byte, long>(sizeBytes);
+            return _size[0];
+        }
+
+        async Task<int> encode()
+        {
+            using var input = File.OpenRead(fileName);
+
+            using var outputA = File.Create($"{fileName}.a");
+            using var outputB = File.Create($"{fileName}.b");
+            using var outputP = File.Create($"{fileName}.p");
+            using var outputQ = File.Create($"{fileName}.q");
+
+            var inputInfo = new FileInfo(fileName);
+            var sizeBytes = createSizeBytes(inputInfo.Length);
+
+            await Task.WhenAll(
+                outputA.WriteAsync(sizeBytes, 0, sizeBytes.Length),
+                outputB.WriteAsync(sizeBytes, 0, sizeBytes.Length),
+                outputP.WriteAsync(sizeBytes, 0, sizeBytes.Length),
+                outputQ.WriteAsync(sizeBytes, 0, sizeBytes.Length)
+                );
+
+            for (;;)
+            {
+                var readCount = await input.ReadAsync(buffer);
+                if (readCount == 0)
+                {
+                    break;
+                }
+                
+                await Task.WhenAll(
+
+                    outputA.WriteAsync(buffer, 0, PARITY_SIZE),
+                    outputB.WriteAsync(buffer, PARITY_SIZE, PARITY_SIZE),
+
+                    Task.Run(async () =>
+                    {
+                        Parity.CalculateP(
+                            buffer.AsSpan(0, PARITY_SIZE),
+                            buffer.AsSpan(PARITY_SIZE, PARITY_SIZE),
+                            pBuffer);
+
+                        await outputP.WriteAsync(pBuffer);
+                    }),
+
+                    Task.Run(async () =>
+                    {
+                        Parity.CalculateQ(
+                            buffer.AsSpan(0, PARITY_SIZE),
+                            buffer.AsSpan(PARITY_SIZE, PARITY_SIZE),
+                            qBuffer);
+
+                        await outputQ.WriteAsync(qBuffer);
+                    })
+
+                    );
+            }
+
+            return 0;
+        }
+
+        async Task<int> restore()
+        {
+            using var output = File.Create(fileName);
+
+            var aExists = File.Exists($"{fileName}.a");
+            var bExists = File.Exists($"{fileName}.b");
+            var pExists = File.Exists($"{fileName}.p");
+            var qExists = File.Exists($"{fileName}.q");
+
+            if (!aExists && !bExists && !pExists && !qExists)
+            {
+                Console.Error.WriteLine("File not found");
+                return 1;
+            }
+
+            using var inputA = aExists
+                ? File.OpenRead($"{fileName}.a")
+                : null;
+            using var inputB = bExists
+                ? File.OpenRead($"{fileName}.b")
+                : null;
+            using var inputP = pExists
+                ? File.OpenRead($"{fileName}.p")
+                : null;
+            using var inputQ = qExists
+                ? File.OpenRead($"{fileName}.q")
+                : null;
+
+            using var outputA = aExists
+                ? null
+                : File.Create($"{fileName}.a");
+            using var outputB = bExists
+                ? null
+                : File.Create($"{fileName}.b");
+            using var outputP = pExists
+                ? null
+                : File.Create($"{fileName}.p");
+            using var outputQ = qExists
+                ? null
+                : File.Create($"{fileName}.q");
+
+            var size = -1L;
+            {
+                var sizeBytes = createSizeBytes(size);
+
+                foreach (var _input in new[] { inputA, inputB, inputP, inputQ }
+                    .Where((it) => it is not null))
+                {
+                    await _input!.ReadAsync(sizeBytes);
+                    if (size != -1L & size != (size = createSize(sizeBytes)))
+                    {
+                        throw new Exception("File parts disagree on size");
+                    }
+                }
+
+                foreach (var _output in new[] { outputA, outputB, outputP, outputQ }
+                    .Where((it) => it is not null))
+                {
+                    await _output!.WriteAsync(sizeBytes);
+                }
+            }
+
+            for (var bytesRestored = 0L; bytesRestored < size; bytesRestored += PARITY_SIZE * 2)
+            {
+                if (aExists)
+                {
+                    await inputA!.ReadAsync(buffer.AsMemory(0, PARITY_SIZE));
+                }
+                if (bExists)
+                {
+                    await inputB!.ReadAsync(buffer.AsMemory(PARITY_SIZE, PARITY_SIZE));
+                }
+                if (pExists)
+                {
+                    await inputP!.ReadAsync(pBuffer);
+                }
+                if (qExists)
+                {
+                    await inputQ!.ReadAsync(qBuffer);
+                }
+
+                if (aExists && bExists)
+                {
+                    // Lucky you
+                } else if (aExists && pExists)
+                {
+                    Parity.RestoreBFromAAndP(
+                        buffer.AsSpan(0, PARITY_SIZE),
+                        pBuffer,
+                        buffer.AsSpan(PARITY_SIZE, PARITY_SIZE));
+                } else if (bExists && pExists)
+                {
+                    Parity.RestoreAFromBAndP(
+                        buffer.AsSpan(PARITY_SIZE, PARITY_SIZE),
+                        pBuffer,
+                        buffer.AsSpan(0, PARITY_SIZE));
+                } else if (aExists && qExists)
+                {
+                    Parity.RestoreBFromAAndQ(
+                        buffer.AsSpan(0, PARITY_SIZE),
+                        qBuffer,
+                        buffer.AsSpan(PARITY_SIZE, PARITY_SIZE));
+                } else if (bExists && qExists)
+                {
+                    Parity.RestoreAFromBAndQ(
+                        buffer.AsSpan(PARITY_SIZE, PARITY_SIZE),
+                        qBuffer,
+                        buffer.AsSpan(0, PARITY_SIZE));
+                } else if (pExists && qExists)
+                {
+                    Parity.RestoreBFromPAndQ(
+                        pBuffer,
+                        qBuffer,
+                        buffer.AsSpan(PARITY_SIZE, PARITY_SIZE));
+
+                    Parity.RestoreAFromBAndP(
+                        buffer.AsSpan(PARITY_SIZE, PARITY_SIZE),
+                        pBuffer,
+                        buffer.AsSpan(0, PARITY_SIZE));
+                } else
+                {
+                    Console.Error.WriteLine("He's dead, Jim");
+                    return 1;
+                }
+
+                if (!aExists)
+                {
+                    await outputA!.WriteAsync(buffer.AsMemory(0, PARITY_SIZE));
+                }
+                if (!bExists)
+                {
+                    await outputB!.WriteAsync(buffer.AsMemory(PARITY_SIZE, PARITY_SIZE));
+                }
+                if (!pExists)
+                {
+                    Parity.CalculateP(
+                        buffer.AsSpan(0, PARITY_SIZE),
+                        buffer.AsSpan(PARITY_SIZE, PARITY_SIZE),
+                        pBuffer);
+
+                    await outputP!.WriteAsync(pBuffer);
+                }
+                if (!qExists)
+                {
+                    Parity.CalculateQ(
+                        buffer.AsSpan(0, PARITY_SIZE),
+                        buffer.AsSpan(PARITY_SIZE, PARITY_SIZE),
+                        qBuffer);
+
+                    await outputQ!.WriteAsync(qBuffer);
+                }
+
+                // Result will always be within a 32-bit value
+                var writeCount = (int)Math.Min(PARITY_SIZE * 2, size - bytesRestored);
+                await output.WriteAsync(buffer.AsMemory(0, writeCount));
+            }
+
+            return 0;
+        }
+
+        var start = DateTime.Now;
+
+        var exists = File.Exists(fileName);
+        var exitCode = exists 
+            ? await encode()
+            : await restore();
+
+        var runtime = DateTime.Now - start;
+        Console.WriteLine(runtime.TotalMilliseconds.ToString("#,##0.00") + "ms");
+
+        return exitCode;
+    }
+}
