5923c9bda9c3645aa2bd2e0c89f29877dbec0b35
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2024-09-29 19:45:51 -0500

    Settings file creation and RabbitMQ connection

---
+++ DataVault.Core/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace DataVault.Core;
+
+public class RabbitMQSettings
+{
+    public string HostName { get; set; } = "";
+    public int Port { get; set; }
+    public string UserName { get; set; } = "";
+    public string Password { get; set; } = "";
+}
+
+public class AppSettings
+{
+    public RabbitMQSettings RabbitMQ { get; set; } = new();
+}
--- DataVault/Program.cs
+++ DataVault/Program.cs
@@ -1,15 +1,22 @@
-﻿using DataVault.Core.Node;
+﻿using DataVault.Core;
+using DataVault.Core.Node;
 using DataVault.Core.Queues;
 using DataVault.Core.Syntax;
 using DataVault.Ef;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System.Reflection;
 
 namespace DataVault;
 
 internal static class Program
 {
+    const string SETTINGS_FILE = "appsettings.json";
+    const string DEFAULT_DATA_DIR = "./data/";
+
     static int Main(string[] args)
     {
         if (args.Length > 1)
@@ -17,14 +24,27 @@ internal static class Program
             Console.Error.WriteLine("Usage: ./DataVault.exe [data_folder]");
             return 1;
         }
-        var dataFolder = args.FirstOrDefault("./data/");
+        var dataFolder = args.FirstOrDefault(DEFAULT_DATA_DIR);
 
         try
         {
             Directory.CreateDirectory(dataFolder);
             Directory.SetCurrentDirectory(dataFolder);
 
+            var settingsInfo = new FileInfo(SETTINGS_FILE);
+            if (!settingsInfo.Exists || settingsInfo.Length == 0)
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+
+                using var template = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.{SETTINGS_FILE}")!;
+                using var settingsOut = File.OpenWrite(SETTINGS_FILE);
+
+                template.CopyTo(settingsOut);
+            }
+
             var builder = Host.CreateApplicationBuilder(args);
+
+            builder.Configuration.AddJsonFile(SETTINGS_FILE, false, false);
 
             builder.Services.AddLogging((config) =>
             {
@@ -33,6 +53,30 @@ internal static class Program
 #else
                 config.SetMinimumLevel(LogLevel.Information);
 #endif
+            });
+
+            builder.Services.AddSingleton((sp) =>
+            {
+                var config = sp.GetRequiredService<IConfiguration>();
+                var settings = new AppSettings();
+
+                config.GetSection("RabbitMQ").Bind(settings.RabbitMQ);
+
+                return settings;
+            });
+
+            builder.Services.AddSingleton((sp) =>
+            {
+                var settings = sp.GetRequiredService<AppSettings>();
+                var factory = new ConnectionFactory()
+                {
+                    HostName = settings.RabbitMQ.HostName,
+                    Port = settings.RabbitMQ.Port,
+                    UserName = settings.RabbitMQ.UserName,
+                    Password = settings.RabbitMQ.Password
+                };
+
+                return factory.CreateConnection();
             });
 
             builder.Services.AddDbContext<NodeContext>();
@@ -52,6 +96,8 @@ internal static class Program
 
             var db = app.Services.GetRequiredService<NodeContext>();
             db.Initialize();
+
+            var testRabbit = app.Services.GetRequiredService<IConnection>();
 
             app.WaitForShutdown();
         } catch (Exception ex)
